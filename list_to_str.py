c37951b26609bd051a0876ae1130608b528ba4b3
Chintan Savalia <chintan2011@gmail.com>
2018-07-21 18:42:21 -0400

    convert list to string

---
+++ list_to_str.py
@@ -0,0 +1,27 @@
+def list_to_string(list):
+	return	' '.join(list)
+
+fruits = ['apple','banana','orange','mango','pineapple']
+
+print("This is list;")
+print(fruits)
+
+string = list_to_string(fruits)
+print("This is string;")
+print(string)
+
+# convert List into String
+
+#List
+#fruits = ['apple','banana','orange','mango','pineapple']
+
+#list_to_string = ' '.join(fruits)
+
+#print list
+#print("This is List")
+#print(fruits)
+
+#print string
+#print("This is String")
+#print(list_to_string)
+
